fb1d256ad31902901090db5cdc0b2bec5d3b7500
Alzab1 <zabavasa@tut.by>
2019-12-07 20:53:12 +0300

    fix bugs lesson8

--- lesson8/BookingTest.java
+++ lesson8/BookingTest.java
@@ -1,4 +1,3 @@
-import com.google.common.collect.Iterables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,9 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static org.testng.Assert.*;
 
 public class BookingTest {
     public WebDriver driver;
@@ -27,22 +25,20 @@ public class BookingTest {
 
     @Test
     public void checkAvailabilityMoscow() {
-        driver.findElement(By.xpath(".//input[@type='search']"))
-                .sendKeys("Москва");
+        driver.findElement(By.xpath(".//input[@type='search']")).sendKeys("Москва");
         driver.findElement(By.xpath(".//div[@data-mode='checkin']")).click();
         driver.findElement(By.xpath(".//td[@data-date='2019-12-14']")).click();
         driver.findElement(By.xpath(".//div[@data-mode='checkout']")).click();
         driver.findElement(By.xpath(".//div[@data-mode='checkout']")).click();
         driver.findElement(By.xpath(".//td[@data-date='2019-12-27']")).click();
         driver.findElement(By.xpath(".//button[@data-sb-id='main']")).click();
-        System.out.println(driver.findElement(By.xpath("//h1")).getText());
-        System.out.println(driver.findElement(By.xpath("//h2")).getText());
+        System.out.println(driver.findElement(By.xpath(".//h1")).getText());
+        System.out.println(driver.findElement(By.xpath(".//h2")).getText());
     }
 
     @Test
     public void filterByScore() throws InterruptedException {
-        driver.findElement(By.xpath(".//input[@type='search']"))
-                .sendKeys("Москва");
+        driver.findElement(By.xpath(".//input[@type='search']")).sendKeys("Москва");
         driver.findElement(By.xpath(".//div[@data-mode='checkin']")).click();
         driver.findElement(By.xpath(".//td[@data-date='2019-12-14']")).click();
         driver.findElement(By.xpath(".//div[@data-mode='checkout']")).click();
@@ -51,13 +47,18 @@ public class BookingTest {
         driver.findElement(By.xpath(".//button[@data-sb-id='main']")).click();
         driver.findElement(By.xpath(".//a[@data-category='review_score_and_price']")).click();
         Thread.sleep(10000);
-        WebElement element = driver.findElement(By.xpath(".//a[contains(@class,'sr_cta_button')]"));
+        List<WebElement> webElements = driver.findElements(By.xpath(".//a[contains(@class,'sr_cta_button')]"));
+        WebElement element = webElements.get(0);
         Actions actions = new Actions(driver);
         actions.moveToElement(element).click().perform();
+        ArrayList<String> win2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(win2.get(1));
         String score = driver.findElement(By.xpath(".//a//div[@class='bui-review-score__badge']")).getText();
         int scoreInt = Integer.parseInt(score);
         System.out.println("Рейтинг лучшего отеля " + score);
         System.out.println(scoreInt >= 9 ? "То что надо!" : "Лучше на вокзале");
+        System.out.println(driver.findElement(By.xpath(".//h1")).getText());
+        System.out.println(driver.findElement(By.xpath(".//h2")).getText());
     }
 
     @AfterMethod
